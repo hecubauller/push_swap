917626fc89de3b05ea1e22346bae3c3fc1272773
hecubauller <monina20155@gmail.com>
2019-09-02 13:46:37 +0300

    nedd to check 'b' before pushing

--- includes/checker.h
+++ includes/checker.h
@@ -6,51 +6,7 @@
 /*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 13:24:52 by huller            #+#    #+#             */
-/*   Updated: 2019/08/31 13:24:52 by huller           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/31 13:24:51 by huller            #+#    #+#             */
-/*   Updated: 2019/08/31 13:24:51 by huller           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/31 13:24:51 by huller            #+#    #+#             */
-/*   Updated: 2019/08/31 13:24:51 by huller           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/31 12:42:08 by huller            #+#    #+#             */
-/*   Updated: 2019/08/31 12:45:46 by huller           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: huller <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/12 18:22:01 by huller            #+#    #+#             */
-/*   Updated: 2019/08/30 20:30:58 by huller           ###   ########.fr       */
+/*   Updated: 2019/09/02 13:35:28 by huller           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,6 +95,7 @@ typedef struct			s_alg
 	int 				rra_s;
 	int					res;
 	int 				act_for_f;
+	int 				*ar_of_mx;
 	int 				act_for_s;
 	int 				max; // максимальное значение в массиве
 	int 				min; //минимальное значение в массиве
@@ -151,7 +108,6 @@ typedef struct			s_alg
 
 void		ft_put_result(int res);
 t_stack		*ft_turn_end(t_stack **x);
-void	ft_create_array(int **ar, t_stack **a, t_alg **q, t_instr **in);
 void		ft_free_lsts(t_stack **a);
 void		ft_newlist_ch(t_stack **a);
 t_stack		*ft_turn_begin(t_stack **x);
@@ -159,13 +115,16 @@ int			ft_int_checker(char *tmp, int nb);
 void		ft_sa(t_stack **x, t_instr *instr);
 void		ft_sb(t_stack **x, t_instr *instr);
 void		ft_ra(t_stack **x, t_instr *instr);
-int 	ft_partition(int l, int h, int **ar);
 void		ft_rb(t_stack **x, t_instr *instr);
 void		ft_rrb(t_stack **x, t_instr *instr);
 void		ft_rra(t_stack **x, t_instr *instr);
 int			ft_check_dubl(char **argv, int tmp);
+void	 	ft_how_long(t_instr *in, t_alg **q);
+int 		ft_partition(int l, int h, int *ar);
+void		ft_quick_sort(int *ar, int l, int h);
 int 		ft_check_output(t_stack **a, t_instr *in);
 int 		ft_check_valid_instr(char **line, t_instr *in);
+void 		ft_check_b(t_stack **a, t_stack **b, t_instr *in, t_alg **q);
 void		ft_ss(t_stack **a, t_stack **b, t_instr *instr);
 void		ft_rr(t_stack **a, t_stack **b, t_instr *instr);
 void		ft_pa(t_stack **a, t_stack **b, t_instr *instr);
@@ -179,9 +138,9 @@ int			ft_alg_hundred(t_instr *in, t_stack **a, t_stack **b);
 int			ft_reader_argv(t_stack **a, t_instr **in, char **argv);
 void		ft_vizualize_init(t_stack **a, t_stack **b, t_instr *instr);
 void		ft_add_nbrs(int tmp, t_stack **a, char **argv, t_instr *in);
-void	ft_quick_sort(int **ar, int l, int h);
-void	 	ft_how_long(t_instr *in, t_stack **a, t_stack **b, t_alg **q);
+void		ft_push_up(t_stack **a, t_stack **b, t_instr *in, t_alg **q);
+void		ft_create_array(int *ar, t_stack **a, t_alg **q, t_instr **in);
 void		ft_check_size(t_stack **a, t_stack **b, t_instr **in, t_alg **q);
-void		ft_create_maxs(t_alg **q, int **arofch, t_stack **a, t_instr **in);
+void		ft_create_maxs(t_alg **q, t_stack **a, t_instr **in);
 
 #endif
--- srcs/begin_algorithm.c
+++ srcs/begin_algorithm.c
@@ -6,29 +6,7 @@
 /*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 09:12:59 by huller            #+#    #+#             */
-/*   Updated: 2019/08/31 12:50:01 by huller           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   begin_algorithm.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/29 09:12:58 by huller            #+#    #+#             */
-/*   Updated: 2019/08/29 09:12:58 by huller           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   begin_algorithm.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: huller <huller@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/27 05:13:38 by huller            #+#    #+#             */
-/*   Updated: 2019/08/29 09:05:01 by huller           ###   ########.fr       */
+/*   Updated: 2019/09/02 13:40:57 by huller           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +26,103 @@ t_stack	*ft_turn_begin(t_stack **x)
 	return (*x);
 }
 
-void 	ft_how_long(t_instr *in, t_stack **a, t_stack **b, t_alg **q)
+void	ft_create_array(int *ar, t_stack **a, t_alg **q, t_instr **in)
+{
+	int i;
+
+	i = -1;
+	ft_turn_begin(a);
+	while ((*a) && (*a)->next)
+	{
+		ar[++i] = (*a)->nb;
+		(*a) = (*a)->next;
+	}
+	ar[++i] = (*a)->nb;
+	ft_turn_begin(a);
+}
+
+
+int 	ft_partition(int l, int h, int *ar)
+{
+	int pivot;
+	int i;
+	int tmp;
+	int j;
+
+	i = 1;
+	j = h - 1;
+	tmp = 0;
+	pivot = ar[l];
+	while (i < j)
+	{
+		while (ar[i] <= pivot)
+			i++;
+		while (ar[j] > pivot)
+			j--;
+		if (i < j)
+		{
+			tmp = ar[i];
+			ar[i] = ar[j];
+			ar[j] = tmp;
+		}
+	}
+	tmp = ar[l];
+	ar[l] = ar[j];
+	ar[j] = tmp;
+	return (j);
+}
+
+void	ft_quick_sort(int *ar, int l, int h)
+{
+	int 	j; //end
+	int 	i; //beginning
+
+	if (l < h)
+	{
+		j = ft_partition(l, h, ar);
+		ft_quick_sort(ar, l, j);
+		ft_quick_sort(ar, j + 1, h);
+	}
+}
+
+void	ft_create_maxs(t_alg **q, t_stack **a, t_instr **in)
+{
+	int		cnt;
+	int 	i;
+	int 	*ar;
+	int 	prev_max;
+	int 	cycle;
+
+	cnt = -1;
+	i = -1;
+	cycle = 0;
+	prev_max = (*q)->min;
+	if (!(ar = (int *)malloc(sizeof(int) * (*in)->size_a)))
+		return ;
+	if (!((*q)->ar_of_mx = (int *)malloc(sizeof(int) * (*q)->chunks)))
+		return ;
+	ft_create_array(ar, a, q, in);
+	ft_quick_sort(ar, 0, (*in)->size_a);
+	i = -1;
+	ft_putstr(GRN"sorted array: "RESET);
+	while (++i != 16 )
+		ft_printf("%d ", ar[i]);
+	ft_putstr("\n");
+	i = -1;
+	if ((*q)->chunks == 1) //всего 1 чанк
+		(*q)->ar_of_mx[0] = (*q)->max;
+	else //несколько чанков
+	{
+		while (++cycle < (*q)->chunks)
+		{
+			cnt += 10;
+			(*q)->ar_of_mx[++i] = ar[cnt];
+		}
+		(*q)->ar_of_mx[++i] = (*q)->max;
+	}
+}
+
+void 	ft_how_long(t_instr *in, t_alg **q)
 {
 	(*q)->ra_f = 0;
 	(*q)->ra_s = 0;
@@ -58,7 +132,7 @@ void 	ft_how_long(t_instr *in, t_stack **a, t_stack **b, t_alg **q)
 	(*q)->act_for_f = -1; //what to do for hold_first
 	(*q)->act_for_s = -1; //what to do for hold_second
 	((*q)->place[0] <= ((*in).size_a / 2)) ? ((*q)->act_for_f = RA_IS) :
-		((*q)->act_for_f = RRA_IS);
+	((*q)->act_for_f = RRA_IS);
 	((*q)->place[1] <= ((*in).size_a / 2)) ? ((*q)->act_for_s = RA_IS) :
 	((*q)->act_for_s = RRA_IS);
 	if ((*q)->act_for_f == RA_IS && (*q)->act_for_f == ((*q)->act_for_s)) // 'ra' is faster for both
@@ -92,115 +166,63 @@ void 	ft_how_long(t_instr *in, t_stack **a, t_stack **b, t_alg **q)
 	}
 }
 
-void	ft_create_array(int **ar, t_stack **a, t_alg **q, t_instr **in)
+void 	ft_check_b(t_stack **a, t_stack **b, t_instr *in, t_alg **q)
 {
-	int i;
-
-	i = -1;
-	while ((*a) && (*a)->next)
+	if (!(*b))
 	{
-		*ar[++i] = (*a)->nb;
-		(*a) = (*a)->next;
-	}
-	*ar[++i] = (*a)->nb;
-	ft_turn_begin(a);
-}
-
-
-int 	ft_partition(int l, int h, int **ar)
-{
-	int pivot;
-	int i;
-	int tmp;
-	int j;
-
-	i = 0;
-	j = h;
-	tmp = 0;
-	pivot = *ar[l];
-	while (i < j)
-	{
-		while (*ar[i] <= pivot || *ar[j] > pivot)
-			*ar[i] <= pivot ? i++ : j--;
-		if (i < j)
-		{
-			tmp = *ar[i];
-			*ar[i] = *ar[j];
-			*ar[j] = tmp;
-		}
-		tmp = *ar[l];
-		*ar[l] = *ar[j];
-		*ar[j] = tmp;
-		return (j);
-	}
-
-}
-void	ft_quick_sort(int **ar, int l, int h)
-{
-	int 	j; //end
-	int 	i; //beginning
-	int 	tmp;
-	int 	p;
-
-	tmp = 0;
-	if (l < h)
-	{
-		j = ft_partition(l, h, ar);
-		ft_quick_sort(ar, l, j);
-		ft_quick_sort(ar, j + 1, h);
-	}
-}
-
-void	ft_create_maxs(t_alg **q, int **arofch, t_stack **a, t_instr **in)
-{
-	int		cnt;
-	int 	i;
-	int 	*ar;
-	int 	prev_max;
-	int 	res;
-
-	cnt = 10;
-	i = -1;
-	res = 0;
-	prev_max = (*q)->min;
-	if (!(ar = (int *)malloc(sizeof(int) * (*in)->size_a)))
+		ft_pb(a, b, in);
 		return ;
-	ft_create_array(&ar, a, q, in);
-	ft_quick_sort(&ar, 0, (*in)->size_a);
-	while (++i <= (*q)->chunks)
-	{
-		res = 0;
-		i = 0;
-		cnt = 10;
-		while (--cnt) //10 numbers
-		{
-			while (*a)
-			{
-
-			}
-		}
-		*arofch[i] = prev_max;
 	}
-	ft_turn_begin(a);
+	else
+	{
+		while ((*a)->next)
+		{
+
+		}
+	}
+}
+
+void	ft_push_up(t_stack **a, t_stack **b, t_instr *in, t_alg **q)
+{
+	if ((*q)->res == F_RA || (*q)->res == F_RRA)
+	{
+		while (((*q)->res == F_RA) && (*a)->nb != (*q)->hold_first)
+			ft_ra(a, in);
+		while (((*q)->res == F_RRA) && (*a)->nb != (*q)->hold_first)
+			ft_rra(a, in);
+	}
+	if ((*q)->res == S_RA || (*q)->res == S_RRA)
+	{
+		while (((*q)->res == S_RA) && (*a)->nb != (*q)->hold_first)
+			ft_ra(a, in);
+		while (((*q)->res == S_RRA) && (*a)->nb != (*q)->hold_first)
+			ft_rra(a, in);
+	}
+	ft_check_b(a, b, in, q); //push to b
 }
 
 void	ft_check_size(t_stack **a, t_stack **b, t_instr **in, t_alg **q)
 {
 	int 	cnt_chunk;
 	int 	prev_nb;
-	int 	*arofch;
+	int 	minimum;
 	int 	i;
+	int 	x;
+	int 	y;
 
 	i = 0;
 	(*q)->max = 0;
-	(*q)->min = 0;
+	(*q)->min = (*a)->nb;
+	minimum = (*q)->min;
 	prev_nb = 0;
-	(*q)->chunks = ((*in)->size_a % 10) ? (*in)->size_a / 10 + 1 : (*in)->size_a / 10; //количество чанков
-	if (!(arofch = (int *)malloc(sizeof(int) * (*q)->chunks)))
-		return ;
+	x = 0;
+	y = 0;
+	(*q)->chunks = ((*in)->size_a % 10) ? (*in)->size_a / 10 + 1 :
+				(*in)->size_a / 10; //количество чанков
 	(*q)->cnt_up = *a; //счетчик сверху
 	(*q)->cnt_dwn = ft_turn_end(a); //счетчик низу
 	cnt_chunk = 10;
+	ft_turn_begin(a);
 	while ((*a) && (*a)->next) //поиск максимума
 	{
 		((*a)->nb > (*q)->max) ? ((*q)->max = (*a)->nb) : 0;
@@ -212,28 +234,41 @@ void	ft_check_size(t_stack **a, t_stack **b, t_instr **in, t_alg **q)
 		((*a)->nb < (*q)->min) ? ((*q)->min = (*a)->nb) : 0;
 		(*a) = (*a)->next;
 	}
-	ft_create_maxs(q, &arofch, a, in); //создание max's чанков
+	ft_create_maxs(q, a, in); //создание max's чанков
 	prev_nb = (*q)->min;
+	minimum = (*q)->min;
+	int s = -1;
+	int bj = 3;
+	ft_putstr(RED "maximums: "RESET);
+	while (--bj)
+		ft_printf("%d ", (*q)->ar_of_mx[++s]);
 	while ((*in)->size_a) //пока стек А не будет пустым
 	{
-		cnt_chunk = 10;
+		ft_turn_begin(a);
 		(*q)->cnt_up = (*a); //счетчик сверху
 		(*q)->cnt_dwn = ft_turn_end(a); //счетчик низу
-		while ((*q)->cnt_up != (*q)->cnt_dwn && --cnt_chunk) // пока счетчики не пересекутся
+		while ((*q)->cnt_up != (*q)->cnt_dwn && (!x || !y)) // пока счетчики не пересекутся
 		{
-			if ((*q)->cnt_up->nb >= (*q)->min && (*q)->cnt_up->nb <= arofch[i] &&
-					(*q)->cnt_up->nb >= prev_nb)// ? prev_nb
+			x = 0;
+			y = 0;
+			if ((*q)->cnt_up->nb >= minimum && (*q)->cnt_up->nb <= (*q)->ar_of_mx[i])
 			{
-				;
+				(*q)->hold_first = (*q)->cnt_up->nb;
+				x = 1;
 			}
 			else
 				(*q)->cnt_up = (*q)->cnt_up->next;
-			if ((*q)->cnt_dwn->nb >= (*q)->min && (*q)->cnt_dwn->nb <= arofch[i] &&
-				(*q)->cnt_dwn->nb > prev_nb)
-				;
+			if ((*q)->cnt_dwn->nb >= minimum && (*q)->cnt_dwn->nb <= (*q)->ar_of_mx[i])
+			{
+				(*q)->hold_second = (*q)->cnt_dwn->nb;
+				y = 1;
+			}
 			else
 				(*q)->cnt_dwn = (*q)->cnt_dwn->prev;
 		}
+		ft_how_long(*in, q); //какое значение оптимальнее двигать вверх
+		ft_turn_begin(a);
+		ft_push_up(a, b, *in, q);
 	}
 }
 
@@ -254,41 +289,7 @@ int		ft_alg_hundred(t_instr *in, t_stack **a, t_stack **b)
 	q->hold_second = 0;
 	q->place[0] = -1;
 	q->place[1] = -1;
-
 	ft_check_size(a, b, &in, &q);
-	while ((*a)->prev)
-		(*a) = (*a)->prev;
-	ft_how_long(in, a, b, &q); //какое значение оптимальнее двигать вверх
-	if ((*q).res == F_RA || (*q).res == F_RRA)
-	{
-		while (((*q).res == F_RA) && (*a)->nb != q->hold_first)
-			ft_ra(a, in);
-		while (((*q).res == F_RRA) && (*a)->nb != q->hold_first)
-			ft_rra(a, in);
-	}
-	if ((*q).res == S_RA || (*q).res == S_RRA)
-	{
-		while (((*q).res == S_RA) && (*a)->nb != q->hold_first)
-			ft_ra(a, in);
-		while (((*q).res == S_RRA) && (*a)->nb != q->hold_first)
-			ft_rra(a, in);
-	}
-	if ((*b) && (*a) && (*b)->nb < (*a)->nb)
-		ft_pb(a, b, in); //push to Stack B
-	else
-	{
-		ft_pb(a, b, in);
-		ft_sb(b, in);
-	}
-	while (((*a) && (*a)->next) || ((*b) && (*b)->next))
-	{
-		ft_putstr("\n");
-		(*a) ? ft_printf("a: %d ", (*a)->nb) : ft_printf("a: ");
-		(*b) ? ft_printf("\nb: %d ", (*b)->nb) : ft_printf(" ");
-		(*a) = (*a)->next;
-		(*b) = (*b)->next;
-	}
-	}
 	return (SUCCESS);
 }
 
